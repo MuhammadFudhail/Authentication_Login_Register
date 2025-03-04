5360aa475add2898de90540ec0b7b619665e0aa4
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2025-03-04 11:09:05 +0800

    first commit

---
+++ backend/src/components/Dashboard.js
@@ -0,0 +1,105 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from 'react-router-dom';
+
+const Dashboard = () => {
+    const [name, setName] = useState('');
+    const [token, setToken] = useState('');
+    const [expire, setExpire] = useState('');
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await refreshToken();
+            getUser()
+        }
+        fetchData()
+    }, []);
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/token');
+            setToken(response.data.accessToken);
+            
+            const decoded = jwtDecode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+        } catch (error) {
+            console.error("Failed to refresh token:", error);
+            navigate('/');
+        }
+    };
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            try {
+                const response = await axios.get('http://localhost:5000/token');
+                setToken(response.data.accessToken);
+                
+                const decoded = jwtDecode(response.data.accessToken);
+                setName(decoded.name);
+                setExpire(decoded.exp);
+                
+                config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            } catch (error) {
+                console.error("Failed to update token:", error);
+                navigate('/');
+            }
+        } else {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+
+    const getUser = async () => {
+        try {
+            const response = await axiosJWT.get('http://localhost:5000/users', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setUsers(response.data)
+            console.log(response.data);
+        } catch (error) {
+            console.error("Failed to fetch user data:", error);
+        }
+    };
+
+    return (
+        <div className='container mt-5'>
+            <h1>Welcome Back: {name}</h1>
+            <button onClick={getUser} className='button is-info'>Get User</button>
+            <table className='table is-striped is-fullwidth'>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                         <tr key={user.id}>
+                         <td>{index + 1}</td>
+                         <td>{user.name}</td>
+                         <td>{user.email}</td>
+                     </tr>
+                    ))}
+                   
+                </tbody>
+            </table>
+
+
+
+        </div>
+    );
+};
+
+export default Dashboard;
